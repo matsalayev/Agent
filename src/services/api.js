dab4ef1e615beeaa0aac34c0efd545538e569e31
nodirbek jumaniyozov <njumaniyozov001@gmail.com>
2025-01-27 21:45:32 +0500

    remove base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'https://4454-213-230-87-101.ngrok-free.app/agent',
+  baseURL: '.../agent',
   headers: {
     'Content-Type': 'application/json',
   },
